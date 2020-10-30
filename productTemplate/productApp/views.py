e0d088253a1df48707bded8fc4f4db6e478547b6
premnath <premdevapp@gmail.com>
2020-10-30 12:51:19 +0530

    new project for product added++

--- productTemplate/productApp/views.py
+++ productTemplate/productApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def electronics(request):
+    product_dict = {'product1':'mac', 'product2':'iphone', 'product3':'dell'}
+    return render(request, 'productApp/productinfo.html', context=product_dict)
+
+def toys(request):
+    product_dict = {'product1':'remote car', 'product2':'drones', 'product3':'rocket'}
+    return render(request, 'productApp/productinfo.html', context=product_dict)
+
+def shoes(request):
+    product_dict = {'product1':'niki', 'product2':'adadas', 'product3':'rebook'}
+    return render(request, 'productApp/productinfo.html', context=product_dict)
+
+def index(request):
+    return render(request, 'productApp/index.html')
